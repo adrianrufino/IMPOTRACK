1647b48131a43cb3d316fe700b82463239b71e68
Adrián Rufino <98433626+adrianrufino@users.noreply.github.com>
2026-04-24 19:48:53 -0300

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,17 +7,29 @@ export async function GET(request: Request) {
   const code = searchParams.get('code')
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type')
-
   const cookieStore = cookies()
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies: { getAll() { return cookieStore.getAll() }, setAll(c) { c.forEach(({ name, value, options }) => cookieStore.set(name, value, options)) } } }
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          for (const cookie of cookiesToSet) {
+            cookieStore.set(cookie.name, cookie.value, cookie.options)
+          }
+        }
+      }
+    }
   )
 
   if (code) {
     await supabase.auth.exchangeCodeForSession(code)
   } else if (token_hash && type) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     await supabase.auth.verifyOtp({ token_hash, type: type as any })
   }
 
